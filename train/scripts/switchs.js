597bb22ca3fe812f7a1b01dabe1ce76fb9617c07
Arthur Santos <arthur.pentester@gmail.com>
2023-09-23 14:26:38 -0300

    more commit

---
+++ train/scripts/switchs.js
@@ -0,0 +1,16 @@
+var id = 5
+
+switch(id){
+    case 1:
+        console.log("aposentado")
+        break
+    case 2:
+        console.log("desempregado")
+        break
+    case 3:
+        console.log("trabalhador formal")
+        break
+    default:
+        console.log("[ERROR] id invalido")
+        break
+}
