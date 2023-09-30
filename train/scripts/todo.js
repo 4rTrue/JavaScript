19cce07ee7ba0d2f0fe378170b871e8a0da251ed
Arthur Santos <arthur.pentester@gmail.com>
2023-09-29 23:57:48 -0300

    new update

--- train/scripts/todo.js
+++ train/scripts/todo.js
@@ -1,16 +1,30 @@
-function inserirElemento(){
+function inserirTarefa(){
     
     // capturando valor
     let input = window.document.getElementById('new-element')
     let text = input.value
+    let br = document.createElement('br')
 
     // teste lógico para inserir texto 
     if (text != '') {
         let container = window.document.getElementById('container')
-        let div = document.createElement('div')
-        div.className = 'element'
-        div.innerText = text
-        container.appendChild(div)
+        let checkbox = document.createElement('input')
+        let label = document.createElement('label')
+                
+        checkbox.setAttribute('type','checkbox')
+        checkbox.setAttribute('name',text)
+        
+        label.setAttribute('for',text)  
+        label.innerText = text      
+
+        container.appendChild(checkbox)
+        container.appendChild(label)
+        container.appendChild(br)
     }
-    console.log(input.value)
+    console.log(text)
+}
+
+function concluirTarefa() {
+    let form = document.getElementById('container')
+    console.log(form.children)
 }
